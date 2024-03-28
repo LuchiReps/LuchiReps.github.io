3abc75a067c63af448167c0bb9dc1f9940b19f9f
LuchiReps <luchieukene@gmail.com>
2024-03-28 18:48:40 -0300

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+let menuVisible = false;
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+
