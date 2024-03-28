8df1414ece3948a8424016e1d53b58697b239963
LuchiReps <luchieukene@gmail.com>
2024-03-28 19:17:00 -0300

    js1

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
+function seleccionar(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
 
-
+function efectoHabilidades()
+{
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades [0].classList.add("javascript"); 
+    }
+}
