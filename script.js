dd479d857d952e74372c5e20cac88d2aef3de5ae
LuchiReps <luchieukene@gmail.com>
2024-03-28 19:51:53 -0300

    js2

--- script.js
+++ script.js
@@ -14,12 +14,20 @@ function seleccionar(){
     menuVisible = false;
 }
 
-function efectoHabilidades()
-{
-    var skills = document.getElementById("skills");
+function efectoHabilidades(){
+    var skills = document.getElementById("barra-skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades [0].classList.add("javascript"); 
+        habilidades[0].classList.add("javascript"); 
+        habilidades[1].classList.add("htmlcss"); 
+        habilidades[2].classList.add("sql"); 
+        habilidades[3].classList.add("htmlcss"); 
+        habilidades[4].classList.add("git_github"); 
+        habilidades[5].classList.add("Comunicacion"); 
+        habilidades[6].classList.add("Trabajo");
+        habilidades[7].classList.add("Creatividad"); 
+        habilidades[8].classList.add("Dedicacion");
+
     }
 }
